244042cc83ba1455cf21a1b7f2a836d672d6864f
Cesar Ramirez <98363353+Cesarex03@users.noreply.github.com>
2022-02-03 11:35:56 -0300

    Camera Update
    Se logro cambiar entre camares, y aplicar cinemamachine a la MainCamera

--- Project-Spartan/Assets/Scripts/CameraControls.cs
+++ Project-Spartan/Assets/Scripts/CameraControls.cs
@@ -15,25 +15,27 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
+        if (Input.GetKeyDown(KeyCode.F1)){
 
             EnableCamera(0,true);
             EnableCamera(1,false);
             EnableCamera(2,false);
+        }
 
-        if (Input.GetKeyDown(KeyCode.F2))
+        if (Input.GetKeyDown(KeyCode.F2)){
 
             EnableCamera(0,false);
             EnableCamera(1,true);
             EnableCamera(2,false);
-
-        if (Input.GetKeyDown(KeyCode.F3))
+        }
+        if (Input.GetKeyDown(KeyCode.F3)){
 
             EnableCamera(0,false);
             EnableCamera(1,false);
             EnableCamera(2,true);
-       
+        }
     }
+    
     void EnableCamera(int position, bool status){
 
         cameras[position].SetActive(status);
